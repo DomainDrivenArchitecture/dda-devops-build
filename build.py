f7d57c2d5ed899266b0207243e2114a1963acecc
jem <michael.jerger@meissa-gmbh.de>
2019-11-08 20:11:02 +0100

    prep release

--- build.py
+++ build.py
@@ -27,7 +27,7 @@ use_plugin("python.distutils")
 default_task = "publish"
 
 name = "ddadevops"
-version = "0.3.5.dev0"
+version = "0.3.6"
 summary = "tools to support builds combining gopass, terraform, dda-pallet, aws & hetzner-cloud"
 description = __doc__
 authors = [Author("meissa GmbH", "buero@meissa-gmbh.de")]

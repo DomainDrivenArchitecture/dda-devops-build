66111e5ad894122ec8d4fab0c7ba928509041b48
jem <michael.jerger@meissa-gmbh.de>
2020-01-28 12:47:43 +0100

    fix env_path refactoring

--- src/main/python/ddadevops/credential.py
+++ src/main/python/ddadevops/credential.py
@@ -2,14 +2,14 @@ from subprocess import check_output, call
 import os
 import sys
 
-def gopass_credential_from_env_path (env):
-    env_path = os.environ.get(env, None)
+def gopass_credential_from_env_path (env_var):
+    env_path = os.environ.get(env_var, None)
     return gopass_credential_from_path(env_path)
 
 def gopass_credential_from_path (path):
     credential = None
 
-    if env_path:
+    if path:
         print('get credential for: ' + path)
         if sys.version_info.major == 3:
             credential = check_output(['gopass', path], encoding='UTF-8')

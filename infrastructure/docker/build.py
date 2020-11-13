1ebf8fe06dfe06ea92da69b75059f615671149ab
jem <michael.jerger@meissa-gmbh.de>
2020-11-13 16:19:47 +0100

    add alpine image for using devops-build on gitlab

---
+++ infrastructure/docker/build.py
@@ -0,0 +1,40 @@
+from subprocess import run
+from os import environ
+from pybuilder.core import task, init
+from ddadevops import *
+import logging
+
+name = 'dda-devops-build'
+MODULE = 'docker'
+PROJECT_ROOT_PATH = '../..'
+
+
+class MyBuild(DevopsDockerBuild):
+    pass
+
+@init
+def initialize(project):
+    project.build_depends_on('ddadevops>=0.6.1')
+    stage = 'notused'
+    dockerhub_user = 'notused'
+    dockerhub_password = 'notused'
+    config = create_devops_docker_build_config(
+        stage, PROJECT_ROOT_PATH, MODULE, dockerhub_user, dockerhub_password)
+    build = MyBuild(project, config)
+    build.initialize_build_dir()
+
+
+@task
+def image(project):
+    build = get_devops_build(project)
+    build.image()
+
+@task
+def drun(project):
+    build = get_devops_build(project)
+    build.drun()
+
+@task
+def test(project):
+    build = get_devops_build(project)
+    build.test()

5b3a19ba62107cc57645f29d376cd8f1c9e13b86
jem <michael.jerger@meissa-gmbh.de>
2021-05-07 18:42:43 +0200

    release

--- src/main/resources/terraform/hetzner_provider.tf
+++ src/main/resources/terraform/hetzner_provider.tf
@@ -1,8 +1,9 @@
 # Configure the Hetzner Cloud Provider
 terraform {
   required_providers {
-    hcloud = {
-      source = "hetznercloud/hcloud"
+     hcloud = {
+      source = "hetznercloud/hcloud" # this whole block is important
+      version = "1.23.0"
     }
   }
   required_version = ">= 0.13"
